d027e5d3e1ee60ba000728754578e34c23aba016
Mariaah <146074601+Mariarosealex@users.noreply.github.com>
2025-01-15 11:23:38 +0530

    Create pr&next.py

---
+++ pr&next.py
@@ -0,0 +1,6 @@
+#Write a program that reads an integer number and prints its previous and next numbers. See the examples below for the exact format your answers should take. There shouldn't be a space before the period.
+a=int(input())
+n=(a+1)
+p=(a-1)
+print('The next number for the number '+str(a)+' is '+str(n))
+print('The previous number for the number '+str(a)+' is '+str(p))
