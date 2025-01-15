5a3007a74681b80e6e0e72f2c2ce4d0351005dd9
Mariaah <146074601+Mariarosealex@users.noreply.github.com>
2025-01-15 11:21:16 +0530

    Create area_of_right_triangle.py

---
+++ area_of_right_triangle.py
@@ -0,0 +1,5 @@
+#Write a program that reads the length of the base and the height of a right-angled triangle and prints the area. Every number is given on a separate line.
+base=int(input())
+height=int(input())
+area=1/2*(base*height)
+print(area)
