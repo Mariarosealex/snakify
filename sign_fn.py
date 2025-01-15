b0f9146895ff43c113801096c7f5619d62c78561
Mariaah <146074601+Mariarosealex@users.noreply.github.com>
2025-01-15 11:44:16 +0530

    Create sign_fn.py

---
+++ sign_fn.py
@@ -0,0 +1,8 @@
+#For the given integer X print 1 if it's positive, -1 if it's negative, or 0 if it's equal to zero.
+x=int(input())
+if x>0:
+    print(1)
+elif x<0:
+    print(-1)
+else:
+    print(0)
