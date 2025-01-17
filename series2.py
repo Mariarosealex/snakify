7df059c5090f8a0c61a27b7a1c4c0a37fda68174
Mariaah <146074601+Mariarosealex@users.noreply.github.com>
2025-01-17 21:47:09 +0530

    Create series2.py

---
+++ series2.py
@@ -0,0 +1,9 @@
+#Given two integers A and B. Print all numbers from A to B inclusively, in ascending order, if A < B, or in descending order, if A ≥ B.
+a=int(input())
+b=int(input())
+if a<b:
+    for i in range(a,b+1):
+        print(i)
+else:
+    for i in range(a,b-1,-1):
+        print(i)
